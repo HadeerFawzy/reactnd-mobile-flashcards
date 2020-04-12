2439098b5d1b31ea7c2cdf135fb58448d5169b4f
Hadeer Fawzy <hadeerfawzymahmoud92@gmail.com>
2020-04-12 22:31:25 +0200

    add, edit colors

--- utils/colors.js
+++ utils/colors.js
@@ -1,4 +1,5 @@
   
 export const white = '#fff';
-export const red = '#c30000';
-export const green = 'darkgreen';
+export const red = '#ad3131';
+export const green = '#03ad9f';
+export const grey = '#777777';
