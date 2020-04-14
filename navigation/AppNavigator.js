25eb82d8d9eb08d3e434b51e31461c507b69dd69
Hadeer Fawzy <hadeerfawzymahmoud92@gmail.com>
2020-04-14 23:01:02 +0200

    create new component to test

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -8,40 +8,75 @@ import { Platform } from 'react-native';
 import * as Icon from '@expo/vector-icons';
 import DecksList from '../components/DecksList';
 import AddDeck from '../components/AddDeck';
+import AddCard from '../components/AddCard';
 import { white, green, red } from '../utils/colors';
 
 const isIOS = Platform.OS === 'ios' ? true : false;
 
-const FlashCardsTabs = createStackNavigator({
+const navTabs = ({
+  Decks: {
+    screen: DecksList,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ green }) => (
+        <Icon.Ionicons
+          name={isIOS ? 'ios-bookmarks' : 'md-bookmarks'}
+          size={30}
+          color={green}
+        />
+      )
+    }
+  },
   AddDeck: {
     screen: AddDeck,
     navigationOptions: {
-      headerTintColor: green,
-      headerStyle: {
-          backgroundColor: green
-      },
-      title: 'Add Deck',
+      tabBarLabel: 'Add Deck',
+      tabBarIcon: ({ green }) => (
+        <Icon.FontAwesome name="plus-square" size={30} color={green} />
+      )
     }
   },
-  DecksList: {
-    screen: DecksList,
-    navigationOptions: {
-      headerTintColor: green,
-      headerStyle: {
-          backgroundColor: green
+},{
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? green : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : green,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
       },
-      title: 'Decks',
+      shadowRadius: 6,
+      shadowOpacity: 1
     }
   }
-  },{
+})
+
+const AppTabs = createBottomTabNavigator(navTabs)
+
+const FlashCardsTabs = createStackNavigator({
+  Home: {
+    screen: AppTabs,
     navigationOptions: {
       header: null
-    },
-    headerTitleAlign: 'center'
-  }
-);
+    }
+  },
+  AddCard: {
+    screen: AddCard,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple,
+      }
+    }
+  },
+});
 
-const AppTabs = createBottomTabNavigator()
+
 
 
 // function CardsTabs() {
